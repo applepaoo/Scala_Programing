16081c9b0c4212cd93840b628181ca48a91164cd
Jeff Chen <Jeff@Jeffde-MacBook-Air.local>
2017-10-01 20:18:34 +0800

    It's working now!!!!

--- src/main/java/Kafka_Producer_Java.java
+++ src/main/java/Kafka_Producer_Java.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 
 import java.util.Properties;
-import java.util.Scanner;
 
 public class Kafka_Producer_Java {
 
@@ -22,64 +21,30 @@ public class Kafka_Producer_Java {
         props.put("enable.auto.commit", "false");
         props.put("auto.offset.reset", "earliest");
         Producer<String, String> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 100; i++) {
+        System.out.println("準備傳送");
 
-            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
 
-          //  System.out.println(i);
+
+
+          for (int i = 0; i < 100; i++) {
+            System.out.println("傳送開始");
+            producer.send(new ProducerRecord<String, String>("TEST", Integer.toString(i), Integer.toString(i)));
+            System.out.println("傳送結束");
+           System.out.println(new ProducerRecord<String, String>("TEST", Integer.toString(i), Integer.toString(i)));
 
         }
 
-        System.out.println("Message sent successfully");
 
-        int input;
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("請輸入數字");
-        input = scanner.nextInt();
+
+
 
         producer.close();
-
-
-
-        /*//Assign topicName to string variable
-        String topicName = "AAA";
-
-        // create instance for properties to access producer configs
-        Properties props = new Properties();
-
-        //Assign localhost id
-        props.put("bootstrap.servers", "140.128.98.31:9092");
-
-        //Set acknowledgements for producer requests.
-        props.put("acks", "all");
-
-        //If the request fails, the producer can automatically retry,
-        props.put("retries", 0);
-
-        //Specify buffer size in config
-        props.put("batch.size", 16384);
-
-        //Reduce the no of requests less than 0
-        props.put("linger.ms", 1);
-
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        props.put("buffer.memory", 33554432);
-
-        props.put("key.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
-
-        props.put("value.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
-
-        Producer<String, String> producer = new KafkaProducer
-                <String, String>(props);
-
-        for(int i = 0; i < 10; i++)
-            producer.send(new ProducerRecord<String, String>(topicName,
-                    Integer.toString(i), Integer.toString(i)));
         System.out.println("Message sent successfully");
-        producer.close();*/
+
+
+
+
 
     }
 }
