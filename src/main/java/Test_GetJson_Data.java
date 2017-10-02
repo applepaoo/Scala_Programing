25f8383498c3630cdd23a7067bbbc93b7eedb3b8
applepaoo <applepaoo@gmail.com>
2017-10-02 20:27:55 +0800

    已抓到電表資料

--- src/main/java/Test_GetJson_Data.java
+++ src/main/java/Test_GetJson_Data.java
@@ -1,7 +1,9 @@
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
@@ -11,26 +13,51 @@ public class Test_GetJson_Data {
 
     public static void main(String[] args) throws JSONException, IOException{
 
+        //先抓網頁API的JSON
+
         URLConnection connection = new URL("http://140.128.197.129:8080/rest/buildingMeter/powerUsage/").openConnection();
-        try(Scanner scanner = new Scanner(connection.getInputStream());){
-            String PowerData = scanner.useDelimiter("\\A").next();
-            System.out.println(PowerData);
-        }
+        Scanner scanner = new Scanner(connection.getInputStream());
 
-        JSONObject j;
+        String PowerData = scanner.useDelimiter("\\A").next();
+        System.out.println(PowerData);
+
+
+        //在測試抓JSON的資料
+
+      /*  JSONArray k;
+        JSONObject i = new JSONObject();
+
         try {
-            String tmp = "{\"Data\":{\"Name\":\"MichaelChan\",\"Email\":\"XXXX@XXX.com\",\"Phone\":[1234567,0911123456]}}";
 
-            j = new JSONObject(tmp);
+            k = new JSONArray(PowerData);
 
-            Object jsonOb = j.getJSONObject("Data").get("Name");
+            i = k.getJSONObject(0);
 
-            System.out.println(jsonOb);
+            System.out.print(i.getString("location") + ",");
+            System.out.println(i.getString("KW"));
+
+
 
         }catch(Exception e){
             System.err.println("Error: " + e.getMessage());
+        }*/
+
+        JSONArray k;
+        JSONObject i = new JSONObject();
+
+        k = new JSONArray(PowerData);
+
+
+        for (int p=0; p<k.length(); p++){
+
+            i = k.getJSONObject(p);
+
+            System.out.print(i.getString("location") + ",");
+            System.out.println(i.getString("KW"));
+
         }
 
+        System.out.println("電表目前共有"+ k.length() + "隻");
 
     }
 
