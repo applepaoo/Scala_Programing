293559457923596ccfc0ff2ffdd11fb9d9aeb5b4
Jeff Chen <Jeff@Jeffde-MacBook-Air.local>
2017-10-12 09:55:32 +0800

    no message

--- build.sbt
+++ build.sbt
@@ -10,6 +10,6 @@ libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.2.0"
 
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "0.10.2.0"
 
-libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0" //% "provided"
+//libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0" //% "provided"
 
 libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.2.0" //% "test"
---
+++ src/main/scala/test_Spark_Streaming_Kafka.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+
+
+object test_Spark_Streaming_Kafka extends App{
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "use_a_separate_group_id_for_each_stream",
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
+
+  val conf = new SparkConf().setAppName("Spark_Streaming_Kafka")
+    .setMaster("local[*]")
+
+  val ssc = new StreamingContext(conf, Seconds(60))
+
+  val topics = Array("topicA", "topicB")
+  val stream = KafkaUtils.createDirectStream[String, String](
+    streamingContext,
+    PreferConsistent,
+    Subscribe[String, String](topics, kafkaParams)
+  )
+
+  stream.map(record => (record.key, record.value))
+
+}
